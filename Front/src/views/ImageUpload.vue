00e87dfca0ff28e626ab8becd4b21acd9e139dbb
devsungmin <kimsungmin.dev@gmail.com>
2020-10-09 23:57:22 +0900

    Add: main / image upload page

---
+++ Front/src/views/ImageUpload.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-content>
+    <v-container>
+      <h2>ImageUpload Page</h2>
+    </v-container>
+  </v-content>
+</template>
+<script>
+export default {
+  name: "ImageUpload",
+};
+</script>
+<style lang="scss">
+</style>
---
+++ Front/src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-content>
+    <v-container>
+      <div class="main-page">
+        <h2>Main Page</h2>
+      </div>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+</style>
