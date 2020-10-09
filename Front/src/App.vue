f1e9aef8eed2320209af10c2942a1210c7222c77
devsungmin <kimsungmin.dev@gmail.com>
2020-10-09 18:47:05 +0900

    Add: vuetify framework

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+   <Nevbar/>
+   
+  </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+import Nevbar from './components/NevBar';
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+export default {
+  name: 'App',
+
+  components: {
+    Nevbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Front/src/components/NevBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="navibar">
+        <v-app-bar absolute color="white">
+            <v-toolbar-title>Traffic accident</v-toolbar-title>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
