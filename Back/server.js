c1e4ccdfb78d2347b1358230b582f967efe63bae
devsungmin <kimsungmin.dev@gmail.com>
2020-10-10 01:08:10 +0900

    Add: image upload server

--- Back/server.js
+++ Back/server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express()
 const port = process.env.port || 3000;
+const multer = require('multer');
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.json({
-        success:true,
+        success: true,
     });
 });
 
-app.listen(port,() =>{
+app.use('/uploadImg', express.static('upload'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'upload/');
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        const fileName = "testimg" + req.params.id + ".jpg";
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({
+    storage: storage
+}).single('myfile');
+
+app.post('/upload/:id', (req, res) => {
+    upload(req, res, err => {
+        if (err) console.log(err);
+    });
+})
+
+app.listen(port, () => {
     console.log('server success!!')
 });
