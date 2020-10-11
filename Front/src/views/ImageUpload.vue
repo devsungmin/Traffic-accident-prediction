4d8b15605b5e528746e828edbcd0ef9d380f5de6
devsungmin <kimsungmin.dev@gmail.com>
2020-10-11 20:26:44 +0900

    Fix: image upload page style

--- Front/src/views/ImageUpload.vue
+++ Front/src/views/ImageUpload.vue
@@ -1,18 +1,22 @@
 <template>
   <v-content>
     <v-container>
-      <h2>ImageUpload Page</h2>
-      <div class="img-upload">
-        <input
-          class="input-img"
-          type="file"
-          multiple="true"
-          name="myfile"
-          @change="uploadImages($event.target.name, $event.target.files)"
-          @drop="uploadImages($event.target.name, $event.target.files)"
-        />
-        <v-icon>mdi-camera</v-icon>
-        <span>image upload</span>
+      <div class="img-upload-page">
+        <h2>ImageUpload Page</h2>
+        <div class="img-upload">
+          <input
+            class="input-img"
+            type="file"
+            multiple="true"
+            name="myfile"
+            @change="uploadImages($event.target.name, $event.target.files)"
+            @drop="uploadImages($event.target.name, $event.target.files)"
+          />
+          <div class="input-img-notice">
+            <v-icon>mdi-camera</v-icon>
+            <span>image upload</span>
+          </div>
+        </div>
       </div>
     </v-container>
   </v-content>
@@ -30,6 +34,7 @@ export default {
       const url = "http://127.0.0.1:3000/upload/1";
       axios.post(url, formData).then((res) => {
         console.log("sucess" + res + "files");
+        this.$router.push("/check");
       });
     },
   },
@@ -37,12 +42,23 @@ export default {
 </script>
 
 <style lang="scss">
+.img-upload-page > h2 {
+  text-align: center;
+}
+
 .img-upload {
   outline: 2px dashed;
-  width: 300px;
-  height: 300px;
+  width: 100%;
+  height: 500px;
   position: relative;
+  background: cornflowerblue;
 }
+
+.input-img-notice {
+  text-align: center;
+  font-size: 30px;
+}
+
 .input-img {
   position: absolute;
   opacity: 0;
