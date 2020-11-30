d2151313c8afe67e678ce41b5c12c53787cad8dd
devsungmin <kimsungmin.dev@gmail.com>
2020-11-30 10:45:35 +0900

    Fix: Modified so that the result value of the model file is displayed in Korean.

--- Back/model.py
+++ Back/model.py
@@ -28,7 +28,8 @@ file_path = sys.argv[1]
 if len(sys.argv) != 2:
     print("Insufficient arguments")
     sys.exit()
-print("File path : " + file_path)
+print("File path : "+ file_path)
+
 
 "전처리 단계 초기화"
 #path_dir = "./service"
@@ -36,7 +37,7 @@ file_list = ['overtaking', 'intersection']
 resize = 300
 result = len(file_list)
 image_list = file_path
-# os.listdir(file_path)
+#os.listdir(file_path)
 images = list()
 labels = list()
 
@@ -45,48 +46,41 @@ labels = list()
 resize = 300
 result = len(file_list)
 image_list = file_path
-# os.listdir(file_path)
+#os.listdir(file_path)
 
 "전처리 시작"
-
-
 def preproc(target_image):
     img = cv2.imread(target_image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    images.append(cv2.resize(img, dsize=(resize, resize),
-                             interpolation=cv2.INTER_LINEAR))
+    images.append(cv2.resize(img ,dsize= (resize,resize), interpolation = cv2.INTER_LINEAR))
     images_np = np.array(images, dtype='float32')/255.0
-    images_np = np.expand_dims(images_np, axis=-1)
+    images_np=np.expand_dims(images_np, axis=-1)
     return images_np
-
 
 "checkpoint loading"
 checkpoint_path = "training_1./"+"traffic2/"+"cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-    checkpoint_path, save_weights_only=True, verbose=1, period=5)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only=True, verbose=1, period=5)
 
 
 def create():
-    inputs = keras.Input(shape=(resize, resize, 1))
+    inputs = keras.Input(shape=(resize,resize,1))
     bn = keras.layers.BatchNormalization()(inputs)
-    conv1 = keras.layers.Conv2D(filters=16, kernel_size=[
-                                3, 3], padding='SAME', activation=tf.nn.relu)(bn)
-    pool1 = keras.layers.MaxPool2D(padding='SAME')(conv1)
-
+    conv1 = keras.layers.Conv2D(filters = 16, kernel_size=[3,3], padding='SAME', activation=tf.nn.relu)(bn)
+    pool1= keras.layers.MaxPool2D(padding='SAME')(conv1)
+    
     bn = keras.layers.BatchNormalization()(pool1)
-    conv2 = keras.layers.Conv2D(filters=32, kernel_size=[
-                                3, 3], padding='SAME', activation=tf.nn.relu)(bn)
+    conv2= keras.layers.Conv2D(filters = 32, kernel_size=[3,3], padding='SAME', activation = tf.nn.relu)(bn)
     pool2 = keras.layers.MaxPool2D(padding='SAME')(conv2)
 
     bn = keras.layers.BatchNormalization()(pool2)
-    conv3 = keras.layers.Conv2D(filters=64, kernel_size=[
-                                3, 3], padding='SAME', activation=tf.nn.relu)(bn)
+    conv3 = keras.layers.Conv2D(filters = 64,kernel_size=[3,3], padding='SAME', activation = tf.nn.relu)(bn)
     pool3_flat = keras.layers.Flatten()(conv3)
 
-    dense3 = keras.layers.Dense(units=256, activation=tf.nn.relu)(pool3_flat)
-    drop3 = keras.layers.Dropout(rate=0.4)(dense3)
-    logits = keras.layers.Dense(units=result, activation=tf.nn.softmax)(drop3)
+    
+    dense3 = keras.layers.Dense(units = 256, activation = tf.nn.relu)(pool3_flat)
+    drop3 = keras.layers.Dropout(rate =0.4)(dense3)
+    logits = keras.layers.Dense(units = result, activation=tf.nn.softmax)(drop3)
     return keras.Model(inputs=inputs, outputs=logits)
 
 
@@ -94,8 +88,8 @@ images = preproc(image_list)
 model = create()
 model.summary()
 model.compile(loss='categorical_crossentropy',
-              optimizer='nadam',
-              metrics=['accuracy'])
+             optimizer='nadam',
+             metrics=['accuracy'])
 
 
 latest = tf.train.latest_checkpoint(checkpoint_dir)
@@ -106,11 +100,13 @@ js1 = dict()
 js = dict()
 file_list.sort()
 
-js["traffic"] = file_list[0]
-js1["traffic"] = file_list[1]
+#js["traffic"] = file_list[0]
+js["traffic"] = "교차로 사고"
+#js1["traffic"] = file_list[1]
+js1["traffic"] = "추월 사고"
 
-js["accuracy"] = acc[0][0]*100
-js1["accuracy"] = acc[0][1]*100
+js["accuracy"] = round(acc[0][0]*100,2)
+js1["accuracy"] = round(acc[0][1]*100,2)
 
 jslist = [js, js1]
 js1 = jslist
@@ -118,5 +114,6 @@ jslist = dict()
 jslist["result"] = js1
 
 
-with open("results/result.json", "w") as json_file:
-    json.dump(jslist, json_file)
+with open("results/result.json","w",encoding="utf-8") as json_file:
+    json.dump(jslist, json_file,ensure_ascii=False)
+
